de350126f54b10478f39c925401a71f00f563b37
smrtrbbt <smrtrbbt42@gmail.com>
2020-05-04 19:10:08 -0300

    Update view to match created templates

--- dev/web/shoal/views.py
+++ dev/web/shoal/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Shoal, Fish
 
 def index(request):
-    return HttpResponse("fish are stronger in a shoal")
+    if(request.GET.get('join')):
+      shoal_id = request.GET.get('shoal_id')
 
-def add_fish(request):
-    return HttpResponse("another one joins the shoal")
+      if Shoal.objects.filter(id=shoal_id):
+          return redirect('/shoal/{0}/'.format(shoal_id))
+
+    if(request.GET.get('create')):
+        shoal_name = request.GET.get('shoal_name')
+        new_shoal = Shoal(name=shoal_name)
+        new_shoal.save()
+
+        return redirect('/shoal/{0}/'.format(new_shoal.id))
+
+    return render(request, 'shoal/index.html')
+
+def shoal(request, shoal_id):
+    this_shoal = Shoal.objects.filter(id = shoal_id)
+    context = {}
+    if (this_shoal):
+        fishs = Fish.objects.filter(shoal = shoal_id)
+        context = {
+            'shoal_name': this_shoal[0].name,
+            'shoal_description': this_shoal[0].description,
+            'fishs': fishs,
+        }
+    return render(request, 'shoal/shoal.html', context)
