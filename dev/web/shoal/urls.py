75f54bf1bc57dc4c0cfff02a5f7708f58123b1e9
smrtrbbt <smrtrbbt42@gmail.com>
2020-05-06 02:57:20 -0300

    Optimize code

--- dev/web/shoal/urls.py
+++ dev/web/shoal/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'shoal'
+
 urlpatterns = [
     # /shoal/
     path('', views.index, name='index'),
 
-    # /shoal/48163264/
+    # /shoal/<shoal_id>/
     path('<int:shoal_id>/', views.shoal, name='shoal'),
+
+    # /shoal/join/
+    path('join/', views.join, name='join'),
+
+    # /shoal/create/
+    path('create/', views.create, name='create'),
 ]
--- dev/web/shoal/views.py
+++ dev/web/shoal/views.py
@@ -1,31 +1,19 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Shoal, Fish
 
 def index(request):
-    if(request.GET.get('join')):
-      shoal_id = request.GET.get('shoal_id')
-
-      if Shoal.objects.filter(id=shoal_id):
-          return redirect('/shoal/{0}/'.format(shoal_id))
-
-    if(request.GET.get('create')):
-        shoal_name = request.GET.get('shoal_name')
-        new_shoal = Shoal(name=shoal_name)
-        new_shoal.save()
-
-        return redirect('/shoal/{0}/'.format(new_shoal.id))
-
     return render(request, 'shoal/index.html')
 
 def shoal(request, shoal_id):
-    this_shoal = Shoal.objects.filter(id = shoal_id)
-    context = {}
-    if (this_shoal):
-        fishs = Fish.objects.filter(shoal = shoal_id)
-        context = {
-            'shoal_name': this_shoal[0].name,
-            'shoal_description': this_shoal[0].description,
-            'fishs': fishs,
-        }
-    return render(request, 'shoal/shoal.html', context)
+    shoal = get_object_or_404(Shoal, id=shoal_id)
+    return render(request, 'shoal/shoal.html', context = { 'shoal': shoal })
+
+def create(request):
+    shoal_name = request.POST['shoal_name']
+    shoal = Shoal(name=shoal_name)
+    shoal.save()
+    return redirect('shoal:shoal', shoal_id=shoal.id)
+
+def join(request):
+    shoal_id = request.POST['shoal_id']
+    return redirect('shoal:shoal', shoal_id)
